1df311417944ad3b9a1621b145f9fdd8026671e1
thadeus <thadeusnyariki94@gmail.com>
2022-10-20 17:36:06 +0300

    Done assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,34 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 
 end
+
+mastiff = Dog.new
+mastiff.name = "Mastiff"
+puts mastiff.name
+
+chihuahua = Dog.new
+chihuahua.name = "Chihuahua"
+puts chihuahua.name
+
+corgi = Dog.new
+corgi.name = "Corgi"
+puts corgi.name
+
+shar_pei = Dog.new
+shar_pei.name = "Shar Pei"
+puts shar_pei.name
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
+p kanye.name
+
+kanye.job = "Doctor"
+p kanye.job
